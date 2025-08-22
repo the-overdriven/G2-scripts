773590aba43494a03b1e6f645178332597417f99
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2025-08-22 02:43:19 +0200

    Fight.d: translate comments

--- Scripts/Content/_intern/Fight.d
+++ Scripts/Content/_intern/Fight.d
@@ -1,35 +1,35 @@
 // *******************************
-// Kampf AI Klasse und Konstanten
+// Combat AI class and constants
 // *******************************
 
-// Reaktionen von NSCs können sein :
-const int MOVE_RUN = 1; // Gegner in meinem Fokus + steht wer dazwischen? (G)
-const int MOVE_RUNBACK = 2; // Gegner in meinem Fokus
+// Reactions of NPCs can be:
+const int MOVE_RUN = 1; // Enemy in my focus + is someone standing in between? (G)
+const int MOVE_RUNBACK = 2; // Enemy in my focus
 const int MOVE_JUMPBACK = 3;
-const int MOVE_TURN = 4; // Immer bis Gegner im Fokus (also nie durch neue Aktion unterbrochen, höchstens durch Gegner-Attacke)
-const int MOVE_STRAFE = 5; // (Richtung wird vom Programm entschieden)
-const int MOVE_ATTACK = 6; // in ComboZone = Combo / im Rennen = Sturmattacke?
+const int MOVE_TURN = 4; // Always until enemy is in focus (so never interrupted by a new action, only by enemy attack)
+const int MOVE_STRAFE = 5; // (direction is decided by the program)
+const int MOVE_ATTACK = 6; // in ComboZone = combo / while running = charge attack?
 const int MOVE_SIDEATTACK = 7;
 const int MOVE_FRONTATTACK = 8;
 const int MOVE_TRIPLEATTACK = 9;
 const int MOVE_WHIRLATTACK = 10;
 const int MOVE_MASTERATTACK = 11;
-const int MOVE_TURNTOHIT = 15; // Version > 0.92 => No Effect
-const int MOVE_PARADE = 17; // (keine Attacke = oben)
+const int MOVE_TURNTOHIT = 15; // Version > 0.92 => no effect
+const int MOVE_PARADE = 17; // (no attack = up)
 const int MOVE_STANDUP = 18;
 const int MOVE_WAIT = 19;
-const int MOVE_WAIT_LONGER = 23; // Achtung: Muss 23 sein, da im Programm noch einige unbenutze Moves zwischen 19 und 22 gibt
-const int MOVE_WAIT_EXT = 24; // Achtung: Muss 24 sein, da im Programm noch einige unbenutze Moves zwischen 19 und 22 gibt
+const int MOVE_WAIT_LONGER = 23; // Attention: Must be 23, since the program still has some unused moves between 19 and 22
+const int MOVE_WAIT_EXT = 24; // Attention: Must be 24, since the program still has some unused moves between 19 and 22
 
-const int MAX_MOVE = 6; // FightMove-Variationen pro FAI-Funktion
+const int MAX_MOVE = 6; // FightMove variations per FAI function
 
-const int MAX_FIGHTAI = 50; // Anzahl vorhandener Fight-AIs // Konstante NICHT redundant im Programmcode, kann hier beliebig verändert werden
+const int MAX_FIGHTAI = 50; // Number of existing fight AIs // Constant NOT redundant in the program code, can be changed arbitrarily here
 
-// ******
-// Klasse
+// ****** 
+// Class
 // ******
 
 class C_FightAI
 {
-	var int move[MAX_MOVE];
+    var int move[MAX_MOVE];
 };
